965bc448833789c0a38a49a75b2e606c8f3a0e27
Fay Vera <fvera.cavalcante@gmail.com>
2021-04-24 21:10:00 -0400

    commit tree c56fec8016dc8a565ea492dd69e657ba6749e550

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map(array, callback) {
+    const r  = []
+    for (let i = 0; i < array.length; i++){
+        r.push(callback(array[i])) 
+    }
+    return r
+}
+
+function reduce(arr, callback, starting){
+    let total; 
+    if (starting){
+        total = starting
+        for (let i = 0; i < arr.length; i++){
+            total = callback(arr[i], total)
+        }
+        return total
+    } else {
+        total = arr[0];
+        for (let i = 1; i < arr.length; i++) {
+            total = callback(arr[i], total);    
+        }
+        return total
+    }
+}
